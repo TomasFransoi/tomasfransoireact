496b6b8722d195d5f98df2a96a258dcfee795d85
Tomas Fransoi <tomasfransoi@gmail.com>
2022-07-23 12:43:43 -0300

    Menu-e-commerce

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+const menu = () =>{
+    return(
+        <nav className= 'Navbar'>
+            <div>
+                <h1>Fransoi Tienda</h1>
+            </div>
+            <div>
+                <button>calcos para bicicleta</button>
+                <button>funda para asientos de bicicleta</button>
+                <button>Productos varios</button>
+            </div>
+        </nav>
+    )
+}
+export default menu
